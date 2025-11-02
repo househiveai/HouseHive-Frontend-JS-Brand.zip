9a7957452585ed10b8fdc39dd919c1ca227dc5de
househiveai <househive.ai@gmail.com>
2025-11-02 16:29:06 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
-const nextConfig = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: 'https://househive-backend.onrender.com',
+  },
+  images: {
+    domains: ['househive-backend.onrender.com'],
+  },
+};
+
 module.exports = nextConfig;
